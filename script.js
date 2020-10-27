662b5942bde2be5f52044889ef57c050898d2429
Dmitry <chakin.2001@mail.ru>
2020-10-27 20:08:28 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+console.log("Привет")
+
+let arr = ["orange.png", "apple.jpeg", "banana.bmb"];
+
+console.log(arr[0]);
+
+// alert("Hello world");
+
+// let a = confirm("Are you pidor?");
+
+// let ab = prompt("What is your name?", "name");
+
+console.log("ya" + " Dima");
+
+let incr = 10,
+    decr = 10;
+    incr++;
+    decr--;
+    console.log(incr);
+    console.log(decr);
+
+    console.log(5%2);
+
+    console.log();
+
+g = 4;
+console.log(g);
